ead3550d6853c5acd4f341165f2498cf553e8c2c
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-11-18 16:08:46 +0000

    attempted sort functionality [failed]

--- src/PopulateScoreboard.jsx
+++ src/PopulateScoreboard.jsx
@@ -1,25 +1,40 @@
 import allCountryScores from "./scores";
+
+function sortScores(arr) {
+  //disabled currently
+  //arr.map((country) => country.scores.sort((a, b) => a - b));
+  return arr;
+}
+
 const PopulateScoreboard = () => {
+  const sortedScores = sortScores(allCountryScores);
   return (
     <div className="tables-wrapper">
-
-      {allCountryScores.map((country, countryIndex) => {
+      {sortedScores.map((country, countryIndex) => {
         return (
           <table key={`${country}-${countryIndex}-table`} className="table">
-
             <caption key={`${country}-caption`} className="table-caption">
               {country.name} - Score Board
             </caption>
 
             <thead>
               <tr>
-                <th key={`${country}-${countryIndex}-name-heading`} className="table-heading">Name</th>
-                <th key={`${country}-${countryIndex}-score-heading`} className="table-heading">Score</th>
+                <th
+                  key={`${country}-${countryIndex}-name-heading`}
+                  className="table-heading"
+                >
+                  Name
+                </th>
+                <th
+                  key={`${country}-${countryIndex}-score-heading`}
+                  className="table-heading"
+                >
+                  Score
+                </th>
               </tr>
             </thead>
 
             <tbody>
-
               {country.scores.map((person, personIndex) => {
                 return (
                   <tr key={`${person}-${personIndex}-table-row`}>
@@ -28,14 +43,12 @@ const PopulateScoreboard = () => {
                   </tr>
                 );
               })}
-
             </tbody>
-
           </table>
         );
       })}
     </div>
   );
-}
+};
 
 export default PopulateScoreboard;
