6cf7cf010b214e3e0380f56ca02a9970c4eada8c
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-11-30 12:37:48 +0000

    level 4 complete
    reminder to self - if statements don't work in jsx you have to use ternaries.

--- src/PopulateScoreboard.jsx
+++ src/PopulateScoreboard.jsx
@@ -1,55 +1,78 @@
+import { useState } from "react";
 import allCountryScores from "./scores";
 
 const sortCountries = () => {
   let newArr = allCountryScores;
   //had to google this line:
   newArr.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
-  
+
   return newArr;
 };
 
 const PopulateScoreboard = () => {
+  const [sortOrder, setSortOrder] = useState("descending");
+  const toggleSortOrder = () => {
+    if (sortOrder === "descending") {
+      setSortOrder("ascending");
+    } else {
+      setSortOrder("descending");
+    }
+  };
+
   const sortedArr = sortCountries();
   return (
-    <div className="tables-wrapper">
-      {sortedArr.map((country, countryIndex) => {
-        return (
-          <table key={`${country}-${countryIndex}-table`} className="table">
-            <caption key={`${country}-caption`} className="table-caption">
-              {country.name} - Score Board
-            </caption>
+    <>
+      <button onClick={toggleSortOrder}>Toggle Sort Order</button>
+      <div className="tables-wrapper">
+        {sortedArr.map((country, countryIndex) => {
+          return (
+            <table key={`${country}-${countryIndex}-table`} className="table">
+              <caption key={`${country}-caption`} className="table-caption">
+                {country.name} - Score Board
+              </caption>
 
-            <thead>
-              <tr>
-                <th
-                  key={`${country}-${countryIndex}-name-heading`}
-                  className="table-heading"
-                >
-                  Name
-                </th>
-                <th
-                  key={`${country}-${countryIndex}-score-heading`}
-                  className="table-heading"
-                >
-                  Score
-                </th>
-              </tr>
-            </thead>
+              <thead>
+                <tr>
+                  <th
+                    key={`${country}-${countryIndex}-name-heading`}
+                    className="table-heading"
+                  >
+                    Name
+                  </th>
+                  <th
+                    key={`${country}-${countryIndex}-score-heading`}
+                    className="table-heading"
+                  >
+                    Score
+                  </th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {country.scores.sort((a, b) => (a.s < b.s ? 1 : b.s < a.s ? -1 : 0)).map((person, personIndex) => {
-                return (
-                  <tr key={`${person}-${personIndex}-table-row`}>
-                    <td key={`${person}-${personIndex}-name`}>{person.n}</td>
-                    <td key={`${person}-${personIndex}-score`}>{person.s}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        );
-      })}
-    </div>
+              <tbody>
+                {country.scores
+                  .sort(
+                    sortOrder === "descending"
+                      ? (a, b) => (a.s < b.s ? 1 : b.s < a.s ? -1 : 0)
+                      : (a, b) => (a.s > b.s ? 1 : b.s > a.s ? -1 : 0)
+                  )
+                  .map((person, personIndex) => {
+                    return (
+                      <tr key={`${person}-${personIndex}-table-row`}>
+                        <td key={`${person}-${personIndex}-name`}>
+                          {person.n}
+                        </td>
+                        <td key={`${person}-${personIndex}-score`}>
+                          {person.s}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
